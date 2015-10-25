30620460ff953e5f5e9331e169b8a99b5a8be5ac
ladrift <cxfyzy@gmail.com>
2015-10-25 16:08:10 +0800

    implement `Selector` class

--- Src/Selector.cpp
+++ Src/Selector.cpp
@@ -1,3 +1,4 @@
+#include <stack>
 #include "Selector.h"
 
 Selector::Selector(std::string head) : m_head(head)
@@ -22,6 +23,11 @@ std::vector<std::map<std::string, Token>*>& Selector::getValueTables()
 	return valuetables;
 }
 
+std::string Selector::getHeadName()
+{
+	return this->m_head;
+}
+
 std::string Selector::writeYourself()
 {
     if (selector_stream.size() == 0) {
@@ -33,7 +39,14 @@ std::string Selector::writeYourself()
     while (readAttribute()) {}
     
     // Produce a string from `attributes`.
-    // TODO
+    std::string output = "";
+    output.append(m_head);
+    output.append(" {\n");
+    for (auto attribute : attributes) {
+        output.append(attribute.first + ": " + attribute.second + ";\n");
+    }
+    output.append("}");
+    return output;
 }
 
 bool Selector::readAttribute() {
@@ -49,9 +62,61 @@ bool Selector::readAttribute() {
 }
 
 std::string Selector::readExpression() {
+    std::string expr = "";
+    for (; selector_stream[pos].getType() != Token::COLON; pos++) {
+        if (selector_stream[pos].getType() == Token::ATKEYWORD) {
+            expr.append(getVar(selector_stream[pos].getLexeme()));
+        } else {
+            expr.append(selector_stream[pos].getLexeme());
+        }
+    }
+    // Skip the semi-colon for next analysis.
+    pos++;
+    return expr;
+}
+
+std::string Selector::getVar(std::string var_name) {
+    std::vector<std::map<std::string, Token> *>::iterator current_map = valuetables.end() - 1;
+    for (; current_map != valuetables.begin()-1; current_map--) {
+        std::map<std::string, Token>::iterator token_itr = (*current_map)->find(var_name);
+        if (token_itr != (*current_map)->end()) {
+            return token_itr->second.getLexeme();
+        }
+    }
     return "";
 }
-std::string Selector::getHeadName()
-{
-	return this->m_head;
+/*
+std::string Selector::expr() {
+    return term() + rest_expr();
 }
+
+std::string Selector::rest_expr() {
+    if (pos == selector_stream.size()) {
+        return "";
+    }
+    switch (selector_stream[pos].getType()) {
+        case Token::ADD:
+            // Read '+'
+            pos ++;
+
+            break;
+        case Token::SUB:
+            pos ++;
+            return "-" + term();
+            break;
+        default:
+            return "";
+            break;
+    }
+}
+
+std::string Selector::term() {
+
+}
+
+std::string Selector::rest_term() {
+}
+
+std::string Selector::factor() {
+}
+*/
